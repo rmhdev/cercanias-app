29c3c9d8535c8295412bbbf34d2b142425c4fda6
Rober Martín H <rmh.dev@gmail.com>
2014-07-22 18:06:31 +0200

    Test app title.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,6 +4,14 @@ describe("Cercanias App", function () {
     browser.get('index.html');
 
     describe('Homepage', function () {
+        it('should have a title', function () {
+            expect(browser.getTitle()).toEqual('Cercanias APP');
+        });
+
+        it('should have a title in the navigation bar', function () {
+            expect(element(by.css('header h1')).getText()).toEqual('Cercanias APP');
+        });
+
         it('should have 12 routes', function () {
             expect(element.all(by.css('ul.routes li')).count()).toBe(12);
         });
